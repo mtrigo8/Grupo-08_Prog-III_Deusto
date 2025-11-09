0f08218e589a2284dd499b168a187d92a48e3733
Ekain <e.aranoa@opendeusto.es>
2025-11-09 13:03:20 +0100

    5 Botones, 4 para ordenar y uno para entrar al buscador, y un jcombobox para ordenar los equipos

--- src/gui/JFrameListaEquipos.java
+++ src/gui/JFrameListaEquipos.java
@@ -7,12 +7,16 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.EventObject;
+import java.util.List;
 import java.util.Vector;
-
+import javax.swing.Action;
+import javax.swing.AbstractAction;
 import javax.swing.AbstractCellEditor;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -24,6 +28,7 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.CellEditorListener;
@@ -40,6 +45,7 @@ import domain.Liga;
 public class JFrameListaEquipos extends JFramePadre {
 	private static final long serialVersionUID = 1L;
 	private Liga liga;
+	private JComboBox <String> comboOrdenar;
 	private JTextField filtradoNombre;
 	private JTable tablaEquipos;
 	private DefaultTableModel modeloDatosEquipos;
@@ -58,6 +64,11 @@ public class JFrameListaEquipos extends JFramePadre {
 		panelFiltro.add(new JLabel("Filtrado por nombre: "));
 		panelFiltro.add(filtradoNombre);
 		
+		//El combobox para ordenar la lista de equipos al gusto de cada uno
+		panelFiltro.add(new JLabel("Ordenar por:"));
+		String [] opciones= { "Alfabético (asc)","Alfabético (des)","Titulos (asc)","Titulos (des)"};
+		comboOrdenar=new JComboBox<>(opciones);
+		panelFiltro.add(comboOrdenar);
 		//Crear un panel arriba para añadir el boton y el filtrado por texto
 		JPanel topPanel = new JPanel(new BorderLayout());
 		topPanel.add(panelFiltro);
@@ -86,14 +97,23 @@ public class JFrameListaEquipos extends JFramePadre {
 		//Aplicar listener
 		this.filtradoNombre.getDocument().addDocumentListener(listenerFiltrar);
 		
+		comboOrdenar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				cargarEquiposTablaFiltro(filtradoNombre.getText());
+			}
+		});
+		
 		//Añadir panel y boton 
 		panel.add(BorderLayout.NORTH, topPanel);
-
+		
 		//Dar funcionalidad a botonAtras
 		usoBotonAtras(super.framePrevio);
 		//Inicializar las tablas con los equipos
 		inicializarTabla();
-		cargarEquiposTabla();
+		cargarEquiposTablaFiltro(filtradoNombre.getText());
 		//Insertar la tabla de equipos en un panel con scroll
 		JScrollPane scrollPaneEquipos = new JScrollPane(this.tablaEquipos);
 		scrollPaneEquipos.setBorder(new TitledBorder("Equipos"));
@@ -101,6 +121,74 @@ public class JFrameListaEquipos extends JFramePadre {
 		
 		panel.add(BorderLayout.CENTER, scrollPaneEquipos);
 		this.add(panel);
+		
+		//Definimos la combinación de teclas (Ctrl+F)
+		KeyStroke ctrlF = KeyStroke.getKeyStroke(KeyEvent.VK_F,KeyEvent.CTRL_DOWN_MASK);//IAG el primer ejemplo
+		KeyStroke ctrl1 = KeyStroke.getKeyStroke(KeyEvent.VK_1,KeyEvent.CTRL_DOWN_MASK);
+		KeyStroke ctrl2 = KeyStroke.getKeyStroke(KeyEvent.VK_2,KeyEvent.CTRL_DOWN_MASK);
+		KeyStroke ctrl3 = KeyStroke.getKeyStroke(KeyEvent.VK_3,KeyEvent.CTRL_DOWN_MASK);
+		KeyStroke ctrl4 = KeyStroke.getKeyStroke(KeyEvent.VK_4,KeyEvent.CTRL_DOWN_MASK);
+		//Definimos un nombre único para esta acción (es un simple String)
+        String accionFoco = "focusFilterAction";
+        String accionOrdenAlfabetico_asc = "sortOrdenAlf_asc";
+        String accionOrdenAlfabetico_des = "sortOrdenAlf_des";
+        String accionOrdenTitulos_asc="sortOrdenTit_asc";
+        String accionOrdenTitulos_des="sortOrdenTit_des";
+
+        // 3. Creamos la acción que se ejecutará: poner el foco en el buscador IAG
+        Action accion = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filtradoNombre.requestFocusInWindow();
+            }
+        };
+        Action accionAlf_asc= new AbstractAction() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				comboOrdenar.setSelectedItem("Alfabético (asc)");
+				
+			}
+        	
+        };
+        Action accionAlf_des= new AbstractAction() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				comboOrdenar.setSelectedItem("Alfabético (des)");
+				
+			}
+        	
+        };
+        Action accionTit_asc= new AbstractAction() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				comboOrdenar.setSelectedItem("Titulos (asc)");
+				
+			}
+        };
+        Action accionTit_des= new AbstractAction() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				comboOrdenar.setSelectedItem("Titulos (des)");
+				
+			}
+        };
+     //Vinculamos la tecla (Ctrl+F) con el nombre de la acción ("focusFilterAction") IAG
+        // Usamos WHEN_IN_FOCUSED_WINDOW para que funcione en cualquier parte de la ventana
+        panel.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(ctrlF, accionFoco);
+        panel.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(ctrl1, accionOrdenAlfabetico_asc);
+        panel.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(ctrl2, accionOrdenAlfabetico_des);
+        panel.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(ctrl3, accionOrdenTitulos_asc);
+        panel.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(ctrl4, accionOrdenTitulos_des);
+        // Vinculamos el nombre de la acción ("focusFilterAction") con la acción real (el código a ejecutar) IAG
+        panel.getActionMap().put(accionFoco, accion);
+        panel.getActionMap().put(accionOrdenAlfabetico_asc, accionAlf_asc);
+        panel.getActionMap().put(accionOrdenAlfabetico_des, accionAlf_des);
+        panel.getActionMap().put(accionOrdenTitulos_asc, accionTit_asc);
+        panel.getActionMap().put(accionOrdenTitulos_des, accionTit_des);
 	}
 	private void inicializarTabla() {
 		Vector<String> cabezeraEquipos = new Vector<String>(Arrays.asList("ESCUDO", "NOMBRE"));
@@ -161,30 +249,6 @@ public class JFrameListaEquipos extends JFramePadre {
 		
 	}
 	
-	private void cargarEquiposTabla() {
-		//Borrar datos
-		this.modeloDatosEquipos.setRowCount(0);
-		//Añadir uno a uno al modelo de datos
-		for (Equipo eq: liga.getEquipos()) {
-			botonEquipo = new JButton();
-			botonEquipo.setText(eq.getNombre());
-			//Funcionalidad de abrir la ventana del equipo al boton;
-			botonEquipo.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					//JFramePlantilla jfp = new JFramePlantilla (ligas, liga, eq, JFrameListaEquipos.this);
-					//jfp.setVisible(true);
-					setVisible(false);
-					
-					JFrameEquipo jfe = new JFrameEquipo(eq, JFrameListaEquipos.this);
-					jfe.setVisible(true);
-					//System.out.println("Abrir ventana de equipo: "+eq.getNombre());
-				}
-			});
-			this.modeloDatosEquipos.addRow(new Object[] {eq.getNombrePNGEquipo(),botonEquipo});
-		}
-	}
 
 	//IAG
 	//Funcion que modifica el tamaño de la imagen a la altura de la celda
@@ -208,8 +272,23 @@ public class JFrameListaEquipos extends JFramePadre {
 	//Cargar los equipos teniendo en cuenta el contenido en el JTextField filtradoNombre
 	private void cargarEquiposTablaFiltro (String filtro) {
 		//Borrar los equipos existentes
+		List<Equipo> equiposOrdenados = new ArrayList<>(this.liga.getEquipos());
+		String orden= (String) comboOrdenar.getSelectedItem();
+		if (orden != null && orden.equals("Titulos (asc)")){
+			equiposOrdenados.sort(Comparator.comparingInt(Equipo::getTitulos));//IAG esta line, las demas, teniendo esta en cuenta
+			
+		} else if (orden != null && orden.equals("Titulos (des)")) {
+			equiposOrdenados.sort(Comparator.comparingInt(Equipo::getTitulos).reversed());
+		} else if (orden != null && orden.equals("Alfabético (des)")) {
+			equiposOrdenados.sort(Comparator.comparing(Equipo::getNombre).reversed());
+			
+			//orden alfabetico por defecto
+			
+		} else {
+			equiposOrdenados.sort(Comparator.comparing(Equipo::getNombre));
+		} 
 		modeloDatosEquipos.setRowCount(0);
-		for (Equipo eq: liga.getEquipos()) {
+		for (Equipo eq: equiposOrdenados) {
 			if (eq.getNombre().toLowerCase().contains(filtro.toLowerCase())) {
 				botonEquipo = new JButton(eq.getNombre());
 				botonEquipo.addActionListener(new ActionListener() {
