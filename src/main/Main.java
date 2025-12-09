0baae1462606a86a9f9e11ee9671d3e5de8fa49a
Markel Trigo Cortazar <markel.trigo@opendeusto.es>
2025-12-09 20:21:47 +0100

    commit vacio

--- src/main/Main.java
+++ src/main/Main.java
@@ -30,6 +30,7 @@ public class Main {
 		//Cargar las ligas de la BBDD
 		List<Liga> ligas = new ArrayList<Liga>();
 		ligas = gbd.getLigas();
+		JFramePadre.ligas= (ArrayList<Liga>) ligas;
 		//Cargar los equipos de la BBDD
 		List<Equipo> equipos = new ArrayList<Equipo>();
 		equipos = gbd.getEquipos();
