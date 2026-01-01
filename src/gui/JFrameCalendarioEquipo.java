af5761226dfc7549d2cef4f1c246bd9e4045dd4e
Jon Undabeitia <jon.undabeitia@opendeusto.es>
2026-01-01 19:30:54 +0100

    Calendario personalizado del equipo

--- src/gui/JFrameCalendarioEquipo.java
+++ src/gui/JFrameCalendarioEquipo.java
@@ -18,13 +18,18 @@ public class JFrameCalendarioEquipo extends JFrameCalendario{
 	
 	
 	public JFrameCalendarioEquipo(Liga liga, Equipo equipo, JFramePadre ventanaAnterior) {
-		super(liga, ventanaAnterior);
-		// TODO Auto-generated constructor stub
-		this.liga = liga;
-		this.equipoSeleccionado = equipo;
-		System.out.println(equipo);
+	    super(liga, ventanaAnterior);
+	    this.liga = liga;
+	    this.equipoSeleccionado = equipo;
+	    
+	    // Limpiar la tabla de la carga incorrecta del padre
+	    DefaultTableModel model = (DefaultTableModel) tablaCalendario.getModel();
+	    model.setRowCount(0);
+	    
+	    // Ahora SÍ cargar correctamente
+	    cargarCalendario();
 	}
-	/*
+	
 	@Override
 	public void cargarCalendario() {
 		cargarCalendarioEquipo();
@@ -34,7 +39,7 @@ public class JFrameCalendarioEquipo extends JFrameCalendario{
 		mDatTab = (DefaultTableModel) tablaCalendario.getModel();
 		mDatTab.setRowCount(0);
 		
-		List<Partido> partidosEquipo = obtenerPartidosDelEquipo(equipoSeleccionado);
+		List<Partido> partidosEquipo = obtenerPartidosDelEquipo();
 		for (Partido p : partidosEquipo) {
 			mDatTab.addRow(new Object[] {
 					String.valueOf(p.getJornada()), p.getFecha().toString(), p.getEquipoLocal().getNombre(), p.getEquipoVisitante().getNombre(), 
@@ -43,20 +48,41 @@ public class JFrameCalendarioEquipo extends JFrameCalendario{
 		}
 	}
 
-	private List<Partido> obtenerPartidosDelEquipo (Equipo equipoSeleccionado) {
-		List<Partido> partidosEquipo = new ArrayList<Partido>();
-		
-		TreeMap<Integer, ArrayList<Partido>> calendario = equipoSeleccionado.getLiga().getCalendario();
-		
-		for (int jornada : calendario.keySet()) {
-			for (Partido partido : calendario.get(jornada)) {
-				if (partido.getEquipoLocal().equals(equipoSeleccionado) || partido.getNombreEquipoVisitante().equals(equipoSeleccionado)) {
-					partidosEquipo.add(partido);
-				}
-			}
-		}
-		
-		return partidosEquipo;
-	}
-*/
+	private List<Partido> obtenerPartidosDelEquipo() {
+    List<Partido> partidosEquipo = new ArrayList<Partido>();
+    
+    
+    if (this.equipoSeleccionado == null) {
+        return partidosEquipo; // Retorna lista vacía silenciosamente
+    }
+    
+        
+       // Verificación 2: La liga tiene calendario
+       TreeMap<Integer, ArrayList<Partido>> calendario = this.equipoSeleccionado.getLiga().getCalendario();
+        if (calendario == null) {
+            System.err.println("La liga no tiene calendario inicializado");
+            return partidosEquipo;
+        }
+        
+        for (int jornada : calendario.keySet()) {
+            ArrayList<Partido> partidos = calendario.get(jornada);
+            
+            // Verificación 3: La jornada tiene partidos
+            if (partidos == null) {
+                continue; // Saltamos esta jornada
+            }
+            
+            for (Partido partido : partidos) {
+                // Verificación 4: El partido y sus equipos no son null
+                if (partido != null && partido.getEquipoLocal() != null) {
+                    if (partido.getEquipoLocal().equals(this.equipoSeleccionado) || 
+                        partido.getEquipoVisitante().equals(this.equipoSeleccionado)) {
+                        partidosEquipo.add(partido);
+                    }
+                }
+            }
+        }
+    
+    return partidosEquipo;
+}
 }
